21711682bc1c9545f2bfe5b9ca725ad493d81373
Kalzang dorje lama <kalzanglama77@gmail.com>
2025-09-18 18:17:10 +0545

    edited

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -3,14 +3,14 @@ import { NavLink, Outlet } from "react-router-dom";
 export default function Layout() {
   const linkClass = ({ isActive }) =>
     isActive
-      ? "text-blue-600 font-semibold border-b-2 border-blue-600 font-bold"
-      : "text-gray-700 hover:text-blue-600 font-bold";
+      ? "text-blue-600 font-semibold border-b-2 border-blue-600"
+      : "text-gray-700 hover:text-blue-600";
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-gray-50 text-gray-800">
       {/* Navbar */}
       <nav className="flex justify-between items-center px-10 py-5 bg-white shadow-md sticky top-0 z-50">
-        <h1 className="text-xl font-bold text-blue-700">Deep Nepse</h1>
+        <h1 className="text-2xl font-bold text-blue-700">Deep Nepse</h1>
 
         <div className="flex gap-6">
           <NavLink to="/" className={linkClass} end>
